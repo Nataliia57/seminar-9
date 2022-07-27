389e4671906f4fe29d0b42adf77298707cdd018e
Nataliia <nvasinaa@gmail.com>
2022-07-27 23:00:57 +0300

    задача 64

---
+++ 005/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = 1;
+int n = 15;
+Recursion(m, n);
+Console.WriteLine();
+
+void Recursion(int m, int n, int summa = 0)
+{
+    if (m > n)
+    {
+        Console.Write(summa + " ");
+        return;
+    }
+        summa = summa + m;
+        m++;
+    Recursion(m, n, summa);
+}
+
