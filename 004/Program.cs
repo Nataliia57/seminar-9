2fb69fe6dc2648159e6bcfd7fce632a5a653ba35
Nataliia <nvasinaa@gmail.com>
2022-07-27 22:59:56 +0300

    задача 64

---
+++ 004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N, 
+//кратные трём.
+
+int m =1;
+int n = 21;
+Recursion(m, n);
+Console.WriteLine();
+
+void Recursion(int m, int n)
+{
+    if (m >= n) return;
+    if (m%3 == 0)
+    {
+       Console.Write(m + " "); 
+    }
+    m++;
+    Recursion(m, n);
+}
+
